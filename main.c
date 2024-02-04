c26ffb4832a69843924d807bb1b4c268a54df7d8
nathan <sauret.nathan.pro@gmail.com>
2024-02-04 16:34:12 +0100

    update

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int SIZE = 10;
+
+
+typedef struct snake snake;
+struct snake {
+    int x;
+    int y;
+    snake *next;
+};
+
+
+snake* ajouter_liste(int x, int y, snake *p)
+// add a snake
+{
+    snake *el;
+
+    el = malloc(sizeof(snake));
+    el->x = x;
+    el->y = y;
+    el->next = p;
+    return el;
+}
+
+
+
 int main()
 {
     
